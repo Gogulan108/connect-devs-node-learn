ebf42d8f0274c4890a867c1cf90c8d9ccd483fc2
Gogulan <gogulan.kanagaraj@zf.com>
2025-12-05 15:09:45 +0530

    remove duplicate dotenv configuration

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-require("dotenv").config();
 const express = require("express");
 const app = express(); //instance of express like new application
 const PORT = process.env.PORT || 3000;
